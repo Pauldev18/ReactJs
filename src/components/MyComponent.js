1af5c0ae9c1cfb37b0a7b1c97a32543a8834a565
Pauldev18 <vuquanganh1802@gmail.com>
2023-08-15 22:31:12 +0700

    dom event and setState

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,11 +4,17 @@ class MyComponent extends React.Component{
     name : "Paul dev",
     address: "Hà Nội"
   }
+  handleOnClick = (event) =>{
+    this.setState({
+      name: "Vu Quang Anh"
+    })
+  }
   render(){
     return(
       <div>
         My First Component
         Name is: {this.state.name}
+        <button onClick={(event) => {this.handleOnClick(event)}}>Click Me</button>
       </div>
     )
   }
