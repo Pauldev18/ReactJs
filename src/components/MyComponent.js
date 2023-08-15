d63294295ea6d7cb9efd1fb1babe0b46d5554999
Pauldev18 <vuquanganh1802@gmail.com>
2023-08-15 21:53:48 +0700

    State

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,9 +1,14 @@
 import React from "react";
 class MyComponent extends React.Component{
+  state = {
+    name : "Paul dev",
+    address: "Hà Nội"
+  }
   render(){
     return(
       <div>
         My First Component
+        Name is: {this.state.name}
       </div>
     )
   }
