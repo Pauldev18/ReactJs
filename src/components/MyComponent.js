3c748085d92b5c459eb634a9fd37c439161878a8
Pauldev18 <vuquanganh1802@gmail.com>
2023-08-15 22:59:25 +0700

    form

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -9,12 +9,27 @@ class MyComponent extends React.Component{
       name: "Vu Quang Anh"
     })
   }
+
+  handleOnChange = (event) =>{
+    this.setState({
+      name : event.target.value
+    })
+  }
+  handleOnSubmit = (event) =>{
+    event.preventDefault();
+    console.log(this.state);
+
+  }
   render(){
     return(
       <div>
         My First Component
         Name is: {this.state.name}
         <button onClick={(event) => {this.handleOnClick(event)}}>Click Me</button>
+        <form onSubmit={(event) =>{this.handleOnSubmit(event)}}>
+          <input type="text" onChange={(event) => {this.handleOnChange(event)}}/>
+          <button>Submit</button>
+        </form>
       </div>
     )
   }
