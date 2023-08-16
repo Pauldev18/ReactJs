53874b7b2b599053b658d39d634bde50a5aabe2f
Pauldev18 <vuquanganh1802@gmail.com>
2023-08-16 22:20:05 +0700

    An Hien list

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,14 +1,44 @@
 import React from "react";
-class DisplayInfo extends React.Component{
-  render(){
-    const {name, age} = this.props;
-    return(
+class DisplayInfo extends React.Component {
+  state = {
+    isCheck: true,
+  };
+
+  showHidden = () => {
+    this.setState({
+      isCheck: !this.state.isCheck,
+    });
+  };
+  render() {
+    const { listUsers } = this.props;
+    return (
       <div>
-        My name is {name}
-        <br/>
-        age is {age}
+        <div>
+          <span
+            onClick={() => {
+              this.showHidden();
+            }}
+          >
+            {this.state.isCheck === true ? "Ẩn List" : "Hiện List"}
+          </span>
+        </div>
+        <hr />
+        {this.state.isCheck && (
+          <div>
+            {listUsers.map((user) => {
+              return (
+                <div key={user.id} className={user.age > 20 ? "red" : "green"}>
+                  name: {user.name}
+                  <br />
+                  age: {user.age}
+                  <hr />
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
-    )
+    );
   }
 }
 export default DisplayInfo;
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,12 +2,26 @@ import React from "react";
 import InfoComponent from "./InfoComponent";
 import DisplayInfo from "./DisplayInfo";
 class MyComponent extends React.Component{
+  state={
+    listUsers : [
+      {
+        id:1,
+        name: "user1",
+        age: 20
+      },
+      {
+        id:2,
+        name: "user2",
+        age: 30
+      }
+    ]
+  }
   render(){
     return(
       <div>
        <InfoComponent></InfoComponent>
        <hr></hr>
-       <DisplayInfo name= "props" age = {20}/>
+       <DisplayInfo listUsers = {this.state.listUsers}/>
       </div>
     )
   }
