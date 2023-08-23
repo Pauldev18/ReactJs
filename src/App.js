ac752b439bbe73729bdb8ee00f6dd1b555cce0ec
Pauldev18 <vuquanganh1802@gmail.com>
2023-08-23 23:32:14 +0700

    react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,40 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import MyComponent from './components/MyComponent';
+import './App.scss';
 import React from 'react';
-class App extends React.Component{
-  render(){
-    return(<div>
-      Hello World
-      <MyComponent></MyComponent>
-    </div>)
-  }
+import Header from './components/Header/Header';
 
+
+function App() {
+  return (
+    <div className='app-container'>
+      <Header/>
+    </div>
+
+  );
 }
-// function App() {
-//   return (
-//     <div>
-//       Hello World
-//       <MyComponent></MyComponent>
-//     </div>
-//     // <div className="App">
-//     //   <header className="App-header">
-//     //     <img src={logo} className="App-logo" alt="logo" />
-//     //     <p>
-//     //       Hello World
-//     //     </p>
-//     //     <a
-//     //       className="App-link"
-//     //       href="https://reactjs.org"
-//     //       target="_blank"
-//     //       rel="noopener noreferrer"
-//     //     >
-//     //       Learn React
-//     //     </a>
-//     //   </header>
-//     // </div>
-
-//   );
-// }
 
 export default App;
