e9b3b5d487930a84a3fdee43d9d4e2be139a5fcd
Pauldev18 <vuquanganh1802@gmail.com>
2023-08-24 22:22:06 +0700

    router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from './components/Header/Header';
 
 
+
 function App() {
   return (
     <div className='app-container'>
---
+++ src/components/Admin/Admin.js
@@ -0,0 +1,9 @@
+import React from 'react'
+const Admin = ()=>{
+  return(
+    <div>
+      <h1>Đây là admin component</h1>
+    </div>
+  )
+}
+export default Admin;
